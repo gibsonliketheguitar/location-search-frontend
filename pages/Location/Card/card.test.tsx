e099de16e7c26506ae752b30753affcbab58f594
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2023-01-03 21:05:07 -0800

    add card test

--- pages/Location/Card/card.test.tsx
+++ pages/Location/Card/card.test.tsx
@@ -2,5 +2,28 @@ import React from "react";
 
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import { LocationCard } from "./card";
 
-describe("Photo Card", () => {});
+describe("Location Card", () => {
+  let name: string;
+  let address: string;
+  let rating: number;
+  beforeEach(() => {});
+
+  test("should render given name, address, and rating", () => {
+    name = "Pizza King";
+    address = "123 North King Street";
+    rating = 5;
+
+    render(<LocationCard name={name} address={address} rating={rating} />);
+
+    const storeName = screen.getByText(name);
+    expect(storeName).toBeDefined();
+
+    const storeAddress = screen.getByText(address);
+    expect(storeAddress).toBeDefined();
+
+    const storeRating = screen.getByText("5");
+    expect(storeRating).toBeDefined();
+  });
+});
